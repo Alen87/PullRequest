c9cb356ece7b0de5d278c9e403d11ef12e0842c9
Dino Ramač <64275745+dinoramac@users.noreply.github.com>
2022-09-25 23:06:44 +0200

    Promjena 1

--- PullRequest/src/main/java/com/alen/pullrequest/PullRequest.java
+++ PullRequest/src/main/java/com/alen/pullrequest/PullRequest.java
@@ -13,5 +13,7 @@ public class PullRequest {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        
+        System.out.println("Promjena broj 1");
     }
 }
